1fd9a80fc10e68f8c13bb04caf6c4e007ce4f269
Bastien <bastien.nicoud@gmail.com>
2019-01-16 11:20:09 +0100

    Connexion form

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,50 @@
 <template>
-  <h1>Login</h1>
+  <section class="hero is-light is-bold is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Connexion
+        </h1>
+        <div class="columns">
+
+          <div class="column is-half">
+
+            <b-field
+              label="Nom d'utilisateur">
+              <b-input
+                value="johnsilver"
+                icon="account"
+                size="is-medium"
+                maxlength="30"/>
+            </b-field>
+
+            <b-field
+              grouped>
+              <b-field>
+                <b-input
+                  type="password"
+                  icon="lock"
+                  value="iwantmytreasure"
+                  size="is-medium"
+                  password-reveal/>
+              </b-field>
+              <b-field>
+                <button class="button is-info is-medium">Search</button>
+              </b-field>
+            </b-field>
+
+            <button
+              class="button is-light">
+              <b-icon icon="check"/>
+              <span>Connexion</span>
+            </button>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>

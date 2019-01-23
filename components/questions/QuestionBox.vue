9c32fdc7c457ca921bf54c22ffb5407563bdd8dc
Bastien <bastien.nicoud@gmail.com>
2019-01-23 18:46:34 +0100

    Display question aswer in a little tag box

--- components/questions/QuestionBox.vue
+++ components/questions/QuestionBox.vue
@@ -5,30 +5,37 @@
         <h2 class="title">
           {{ question.question }}
         </h2>
-        <ul>
-          <li
+        <div class="tags is-medium">
+          <span
             v-for="answer of question.answers"
-            :key="answer.name">
-            <p v-if="answer.value == 'true'">
-              <strong
-                class="has-text-success">
-                {{ answer.name }}
-              </strong>
-            </p>
-            <p v-else>
-              <strong
-                class="has-text-error">
-                {{ answer.name }}
-              </strong>
-            </p>
-          </li>
-        </ul>
+            :key="answer.name"
+            :class="{ 'is-success': answer.value == 'true' }"
+            class="tag">
+            {{ answer.name }}
+          </span>
+        </div>
       </div>
-      <div class="column is-2">
+      <div class="column is-3">
         <p class="has-text-right">
           Crée par : <strong>{{ question.created_by.username }}</strong>
         </p>
       </div>
+    </div>
+    <div class="field has-addons">
+      <p class="control">
+        <nuxt-link
+          :to="{ name: 'dashboard-questions-question', params: { question: question.id }}"
+          class="button is-info">
+          <b-icon icon="content-copy"/>
+          <span>Modifier</span>
+        </nuxt-link>
+      </p>
+      <p class="control">
+        <a class="button is-danger">
+          <b-icon icon="delete"/>
+          <span>Supprimer</span>
+        </a>
+      </p>
     </div>
   </div>
 </template>
---
+++ pages/dashboard/questions/_question.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <h2 class="title">
+            {{ question.question }}
+          </h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $axios, params }) {
+    let datas = await $axios.$get(`/api/questions/${params.question}`)
+    return { question: datas.question }
+  }
+}
+</script>
